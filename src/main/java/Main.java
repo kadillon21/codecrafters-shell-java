a219c94bcaa8fb4c439a2814e3c4f194fd0bdc7f
kadillon21 <kadillon21@gmail.com>
2026-05-30 17:44:45 -0500

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.nextLine() + ": command not found");
     }
 }
